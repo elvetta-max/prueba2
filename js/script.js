ef5b0eb342f311aae0f501d47bc8aa18b6b27258
SOUFIANE <elvetta26@gmail.com>
2026-04-29 15:50:20 +0200

    Done Prueba2

--- js/script.js
+++ js/script.js
@@ -31,42 +31,159 @@ function calcularNotas() {
     }
 }
 
-calcularNotas()
-
-
-
-/****
- * Ejercicio 2
-Declara una función que acepte un nombre como parámetro 
-y devuelva el número de palabras que tiene el nombre.
-Ejemplo: Para "Antonio Alberto Jesús" devolverá 3.
- */
-
-function cuentaPalabras(nombre) {
-    let counter = nombre.trim().split(" ").length
-    return `La frase "${nombre}" tiene ${counter} palabras`
-}
-
-console.log(cuentaPalabras("Antonio Alberto Jesús"))
+//calcularNotas()
 
 
 /***
- * Ejercicio 3:
-Diseña un algoritmo que cuente las veces que 
-aparece una determinada letra en una frase.
+ * EJERCICIO 2
+
+Hacer un programa que:
+
+    Ingreso de un numero por teclado
+    Muestre por pantalla si ese numero es de 1 cifra 2 cifras o 3 cifras
+
  */
-//Deseño en la carpeta assets/
 
-function cuentaLetrasEnFrase(frase, letra) {
+function cifrasDeNumero() {
+    let numero = Number(window.prompt(`Engrese un numero`))
 
-    let counter = 0
-    if (frase.trim().length <= 0 || letra.trim().length <= 0) {
-        return `Ingrese frase o letra valida`
+    if (Number(numero)) {
+        window.alert(`Tu numero tiene ${String(numero).length} cifras`)
     } else {
+        window.alert(`No es Numero`)
+    }
 
-     counter = frase.toLowerCase().split("").filter((l => l === letra.toLowerCase())).length
-     return `La letra "${letra}" se repite en la frase "${frase}" ${counter} veces`
+}
+// cifrasDeNumero();
+
+
+/****
+ * 
+EJERCICIO 3
+
+Solicite al usuario que escriba un color. 
+Si ese color existe que lo traduzca al inglés, 
+sino que muestre un mensaje de error
+
+ */
+function colorEnIngles() {
+
+    let color = window.prompt(`Escriba un color`).trim().toLowerCase()
+
+    switch (color) {
+        case "blanco":
+            window.alert(`${color} en Ingles es : WHITE`)
+            break;
+        case "negro":
+            window.alert(`${color} en Ingles es : BLACK`)
+            break;
+        case "verde":
+            window.alert(`${color} en Ingles es : GREEN`)
+            break;
+        case "azul":
+            window.alert(`${color} en Ingles es : BLUE`)
+            break;
+        case "amarillo":
+            window.alert(`${color} en Ingles es : YELLOW`)
+            break;
+        case "marron":
+            window.alert(`${color} en Ingles es : BROWN`)
+            break;
+        case "naranja":
+            window.alert(`${color} en Ingles es : ORANGE`)
+            break;
+        default: window.alert(`Ese color "${color}" NO EXISTE`)
+
     }
 }
+// colorEnIngles()
 
-console.log(cuentaLetrasEnFrase("I like pizza", "z"))
+/**
+ * EJERCICIO 4
+
+Se solicita al usuario que ingrese por teclado 10 números, el programa devolverá:
+
+    Cuantos son positivos
+    Cuantos son negativos
+    Cuantos son pares
+
+ */
+
+function programaNumeros() {
+    let numeros = []
+    let positivos = 0;
+    let negativos = 0;
+    let pares = 0;
+
+    for (let i = 1; i <= 10; i++) {
+        let numero = Number(window.prompt(`Engrese numero ${i}`))
+        if (numero >= 0) {
+            positivos += 1
+        } else {
+            negativos += 1;
+        }
+        if (numero % 2 === 0) {
+            pares += 1
+        }
+        numeros.push(numero)
+    }
+
+    console.log(`La lista de "${numeros}" tiene ${positivos} numeros positivos, ${negativos} numeros negativos, y ${pares} numeros pares`)
+}
+//programaNumeros()
+
+
+/***
+ * EJERCICIO 5
+
+Se solicitan 10 números enteros comprendidos entre 1 y 500, estos números se almacenarán en un array.
+
+    Almacenar todos los números en un array
+    Almacenar en otro array los números comprendidos entre el 1 y 250
+    Almacenar en otro array los números comprendidos entre el 251 y 500
+
+ * 
+ */
+
+function processArrays() {
+    let todosNumeros = [];
+    let listaNumeros1 = [];
+    let listaNumeros2 = [];
+
+    let numero = 0;
+    let counter = 1;
+
+    while (counter <= 10) {
+
+        numero = window.prompt(`Engresa el numero ${counter}`)
+
+        if (!Number(numero) || numero.length === 0) {
+            window.alert(`El numero tiene que ser valido. `);
+        } else if (numero < 1 || numero > 500) {
+            window.alert(`El numero tiene que ser entre 1 y 500`);
+
+        } else {
+            todosNumeros.push(numero)
+            if (Number(numero) <= 250) {
+                listaNumeros1.push(numero)
+            } else {
+                listaNumeros2.push(numero)
+            }
+            counter++
+        }
+    }
+    window.alert(`Todos Los Numeros : "${todosNumeros}"`)
+    window.alert(`Numeros Menores o igual a 250 : "${listaNumeros1}"`)
+    window.alert(`Numeros Mayores a 250 : "${listaNumeros2}"`)
+    counter = 1;
+}
+
+processArrays()
+
+
+
+
+
+
+
+
